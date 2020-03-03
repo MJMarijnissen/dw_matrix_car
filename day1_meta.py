df89e60de82f811daba9e903af6de21d78690020
MJMarijnissen <kubamarijnissen@gmail.com>
2020-03-03 14:03:05 +0100

    Added hist, describe, inuque, mean and 2 groupby charts

--- day1_meta.py
+++ day1_meta.py
@@ -13,3 +13,25 @@ import seaborn as sns
 df = pd.read_hdf("data/car.h5")
 print(df.shape)
 print(df.sample(5))
+
+df.columns.values
+df['price_value'].hist(bins=100)
+df['price_value'].describe()
+df['param_marka-pojazdu'].unique()
+df.groupby('param_marka-pojazdu')['price_value'].mean()
+
+(
+ df
+ .groupby('param_marka-pojazdu')['price_value']
+ .agg(np.median)
+ .sort_values(ascending=False)
+ .head(50)
+ ).plot(kind='bar', figsize=(15,5))
+
+(
+ df
+ .groupby('param_marka-pojazdu')['price_value']
+ .agg([np.mean, np.median, np.size])
+ .sort_values(by='mean', ascending=False)
+ .head(50)
+ ).plot(kind='bar', figsize=(15,5), subplots = True)
