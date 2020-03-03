af67b5b2c1dcfd5377975b25950b2dec54aa58e7
MJMarijnissen <kubamarijnissen@gmail.com>
2020-03-03 13:40:52 +0100

    added import for data visualtizations

--- day1_meta.py
+++ day1_meta.py
@@ -6,6 +6,9 @@ Created on Mon Mar  2 14:38:40 2020
 """
 
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 df = pd.read_hdf("data/car.h5")
 print(df.shape)
