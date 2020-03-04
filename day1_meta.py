03dff553ab62468e18f6519f9c494ade23f537bb
MJMarijnissen <kubamarijnissen@gmail.com>
2020-03-04 14:27:18 +0100

    added imports

--- day1_meta.py
+++ day1_meta.py
@@ -7,8 +7,16 @@ Created on Mon Mar  2 14:38:40 2020
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+#import matplotlib.pyplot as plt
+#import seaborn as sns
+
+from sklearn.dummy import DummyRegressor
+from sklearn.tree import DecisionTreeRegressor
+
+from sklearn.metrics import mean_absolute_error as mae
+from sklearn.model_selection import cross_val_score
+
+import eli5
 
 df = pd.read_hdf("data/car.h5")
 print(df.shape)
