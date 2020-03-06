eb045a491ac562dc419eb5d73fce932a2be1e948
MJMarijnissen <kubamarijnissen@gmail.com>
2020-03-06 09:53:34 +0100

    deleted not needed commented code

--- day1_meta.py
+++ day1_meta.py
@@ -8,15 +8,6 @@ Created on Mon Mar  2 14:38:40 2020
 import pandas as pd
 import numpy as np
 import xgboost as xgb
-#import matplotlib.pyplot as plt
-#import seaborn as sns
-
-
-from sklearn.metrics import mean_absolute_error as mae
-from sklearn.model_selection import cross_val_score, KFold
-
-import eli5
-from eli5.sklearn import PermutationImportance
 
 from hyperopt import hp, fmin, tpe, STATUS_OK
 
@@ -65,26 +56,6 @@ feats = ['param_napęd__cat',
 'feature_łopatki-zmiany-biegów__cat',
 'param_pojemność-skokowa']
 
-# #Decision Tree
-# print("Decision Tree")
-# print(run_model(DecisionTreeRegressor(max_depth = 5), cat_feats))
-
-# #Random Forest
-# print("Random Forest")
-# print(run_model(RandomForestRegressor(max_depth = 5, n_estimators=50, random_state=0), cat_feats))
-
-# #XGBoost
-# xgb_params = {
-#     'max_depth': 5,
-#     'n_estimators': 50,
-#     'learning_rate': 0.1,
-#     'seed': 0
-#     }
-
-# model = xgb.XGBRFRegressor(**xgb_params)
-# print("XGBoost")
-# print(run_model(model, cat_feats))
-
 def obj_func(params):
     print("training with params: ")
     print(params)
@@ -108,35 +79,3 @@ best = fmin(obj_func, xgb_reg_params, algo=tpe.suggest, max_evals=15)
 
 best
 
-# #most influential features
-# X = df[cat_feats].values
-# Y = df['price_value'].values
-# m = xgb.XGBRFRegressor(max_depth=5, n_estimators=50, learning_rate=0.1, seed=0)
-# m.fit(X,Y)
-
-# imp = PermutationImportance(m, random_state=0).fit(X,Y)
-# print(eli5.show_weights(imp, feature_names = cat_feats).data)
-    
-# #from above code come out the most infuential features:
-# feats = ['param_napęd__cat',
-# 'param_skrzynia-biegów__cat',
-# 'param_faktura-vat__cat',
-# 'param_rok-produkcji__cat',
-# 'param_moc__cat',
-# 'param_stan__cat',
-# 'feature_kamera-cofania__cat',
-# 'feature_łopatki-zmiany-biegów__cat',
-# 'param_pojemność-skokowa__cat']
-
-
-# #XGBoost most influantial
-# xgb_params = {
-#     'max_depth': 6,
-#     'n_estimators': 60,
-#     #'learning_rate': 0.1,
-#     'seed': 0
-#     }
-
-# model = xgb.XGBRFRegressor(**xgb_params)
-# print("XGBoost")
-# print(run_model(model, feats))
